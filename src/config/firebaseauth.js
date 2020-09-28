538b532e80162709f724a7a14cb5c84a5201751e
arun rs <arunrs123@gmail.com>
2020-09-28 21:23:12 +0530

    new

--- src/config/firebaseauth.js
+++ src/config/firebaseauth.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import firebase from 'firebase';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAtXAETxe55YLd0S6j0XQEA5EbHKeP0hWQ",
-    authDomain: "chatapp-b1e30.firebaseapp.com",
-    databaseURL: "https://chatapp-b1e30.firebaseio.com",
-    projectId: "chatapp-b1e30",
-    storageBucket: "chatapp-b1e30.appspot.com",
-    messagingSenderId: "741488441955",
-    appId: "1:741488441955:web:ad57c48e0a5ed1d3fe9656",
-    measurementId: "G-ED1BPS3PG2"
-};
 
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+};
 
 const fire = firebase.initializeApp(firebaseConfig);
 
